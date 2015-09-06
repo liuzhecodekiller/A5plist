0f5f0912641982b5d456fcc2cb389282113ae7d6
liuzhe <471340380@qq.com>
2015-09-06 21:33:17 +0800

    dddd

--- A5plist/main.m
+++ A5plist/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  A5plist
 //
-//  Created by qianfeng on 15-6-3.
+//  Created by shenZhenNewWorld on 15-6-3.
 //  Copyright (c) 2015年 sir.liu. All rights reserved.
 //
 
@@ -23,7 +23,7 @@ int main(int argc, const char * argv[])
 #if 0
         //从plist 里面读取数据，容器是字典或是数组（根据plist最外层的类型决定）
         //数组和字典只能从plist 的文件里面读数据，不能从txt中读
-        NSString * path = @"/Users/qianfeng/Desktop/课堂练习/3/3/A5plist/A5plist/QF.plist";
+        NSString * path = @"/Users/shenZhenNewWorld/Desktop/课堂练习/3/3/A5plist/A5plist/QF.plist";
         NSDictionary * dic = [NSDictionary dictionaryWithContentsOfFile:path];
         NSLog(@"%@",dic);
         
@@ -34,7 +34,7 @@ int main(int argc, const char * argv[])
         
         NSDictionary * dic = [NSDictionary dictionaryWithObjectsAndKeys:@"Tencent",@"compannyNme",@"mahuateng",@"CEO",@[@"qie",@"jiangshang",@"youxi",@"kengdie"],@"tag", nil];
         NSLog(@"%@",dic);
-        [dic writeToFile:@"/Users/qianfeng/Desktop/1.plist" atomically:YES];
+        [dic writeToFile:@"/Users/shenZhenNewWorld/Desktop/1.plist" atomically:YES];
         
         
 #endif
